d822a116dcecf623a7650e8ad9efa11e62eac95e
Ehtisham Khalid <ehtisham.khalid92@gmail.com>
2022-10-10 22:46:00 +0200

    Add Users Model

---
+++ RU/RU/Model/Users.swift
@@ -0,0 +1,85 @@
+//
+//  Users.swift
+//  RU
+//
+//  Created by Ehtisham Khalid on 10.10.22.
+//
+
+import Foundation
+
+// MARK: - Users
+struct Users: Codable {
+    let results: [Result]
+}
+
+// MARK: - Result
+struct Result: Codable {
+    let gender: String
+    let name: Name
+    let location: Location
+    let email: String
+    let login: Login
+    let dob, registered: Dob
+    let phone, cell: String
+    let id: ID
+    let picture: Picture
+    let nat: String
+}
+
+// MARK: - Dob
+struct Dob: Codable {
+    let date: String
+    let age: Int
+}
+
+// MARK: - ID
+struct ID: Codable {
+    let name: String
+    let value: String?
+}
+
+// MARK: - Location
+struct Location: Codable {
+    let street: Street
+    let city, state, country: String
+    let postcode: Int
+    let coordinates: Coordinates
+    let timezone: Timezone
+}
+
+// MARK: - Coordinates
+struct Coordinates: Codable {
+    let latitude, longitude: String
+}
+
+// MARK: - Street
+struct Street: Codable {
+    let number: Int
+    let name: String
+}
+
+// MARK: - Timezone
+struct Timezone: Codable {
+    let offset, timezoneDescription: String
+
+    enum CodingKeys: String, CodingKey {
+        case offset
+        case timezoneDescription = "description"
+    }
+}
+
+// MARK: - Login
+struct Login: Codable {
+    let uuid, username, password, salt: String
+    let md5, sha1, sha256: String
+}
+
+// MARK: - Name
+struct Name: Codable {
+    let title, first, last: String
+}
+
+// MARK: - Picture
+struct Picture: Codable {
+    let large, medium, thumbnail: String
+}
