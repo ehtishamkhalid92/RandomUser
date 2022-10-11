bed39d89ab37878d7e6978a8141dc0c9e2d41310
Ehtisham Khalid <ehtisham.khalid92@gmail.com>
2022-10-11 06:53:00 +0200

    Add delete on Swip

--- RU/RU/Controller/UsersViewController.swift
+++ RU/RU/Controller/UsersViewController.swift
@@ -16,26 +16,15 @@ class UsersViewController: UIViewController {
     //MARK: View Life Cycle.
     override func viewDidLoad() {
         super.viewDidLoad()
-        getDataFromUsers(numberOfItem: 20)
+        let url = URL(string: "https://randomuser.me/api/?results=20")!
+        getDataFromUsers(url: url)
         self.tableView.delegate = self
         self.tableView.dataSource = self
         
     }
     
-    //MARK: Actions
-    @IBAction func updateUserButtonPressed(_ sender: UIButton) {
-        print("Stack count", self.usersData.count)
-        if self.usersData.count == 20 {
-            Toast.show(message: "Stack is full please delete one", controller: self)
-        }else {
-            getDataFromUsers(numberOfItem: 20)
-        }
-        
-    }
-    
     //MARK: Functions
-    private func getDataFromUsers(numberOfItem: Int) {
-        let url = URL(string: "https://randomuser.me/api/?results=\(numberOfItem)")!
+    private func getDataFromUsers(url: URL) {
         URLSession.shared.fetchData(at: url) { response in
             DispatchQueue.main.async {
                 switch response {
@@ -54,11 +43,13 @@ class UsersViewController: UIViewController {
     }
 
 }
+
+//MARK: Table View 
 extension UsersViewController : UITableViewDelegate,UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return usersData.count
-     }
-
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! UsersTableViewCell
         guard indexPath.row < usersData.count else {return cell}
@@ -68,7 +59,7 @@ extension UsersViewController : UITableViewDelegate,UITableViewDataSource {
         cell.profileImage.loadImageUsingCache(withUrl: instance.picture.thumbnail)
         return cell
     }
-
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80
     }
@@ -79,5 +70,24 @@ extension UsersViewController : UITableViewDelegate,UITableViewDataSource {
         vc.user = instance
         vc.modalPresentationStyle = .fullScreen
         self.present(vc, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            let alert = UIAlertController(title: "Are you sure?", message: "You want to delete the user?", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { (yes) in
+                // remove the item from the data model
+                self.usersData.remove(at: indexPath.row)
+                // delete the table view row
+                tableView.deleteRows(at: [indexPath], with: .fade)
+                let url = URL(string: "https://randomuser.me/api/")!
+                self.getDataFromUsers(url: url)
+            }))
+            alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil))
+            // show the alert
+            self.present(alert, animated: true, completion: nil)
+            
+        }
     }
 }
